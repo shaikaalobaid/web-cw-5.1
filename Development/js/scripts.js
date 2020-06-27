435b34c1292d469963da6b2f29a64e5aa7f2d275
shaikaalobaid <66521851+shaikaalobaid@users.noreply.github.com>
2020-06-27 21:39:41 +0300

    cw5

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,67 @@
 // السؤال الأول
-// 1
+// 1 
+const name = shaika;
+    
 
 // 2
+let age = 15;
+    
 
 // 3 and 4
+console.log("أنا"+name+""+"و عمري "+age)
+
 
 // 5 and 6
+let five_times_age=5*age;
+console.log(five_times_age)
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobby = ["السباحة", "الكتابة","القراءة"]
+
 
 // 2
+hobby.push("15")
+hobby[3] = 15
+console.log(hobby)
+console.log(hobby.length)
+
 
 // 3
+hobby.pop()
+console.log(hobby)
+
 
 // 4
+let student = {
+    track:"مسار الويب";
+    language:"JavaScript";
+}
+
 
 // 5
+student["TAs"] = ["نانسي"+"موضي"+"حسين"+"رهف"]
+console.log(student)
+
 
 // 6
+console.log(student["track"])
+console.log(student.track)
+
 
 // 7
-
+console.log(student["TAs"][0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
+student["bouns"] = function() {console.log}("مرحبا")
+console.log(student)
+
 
 // 2
+student["bouns"]()
 
 // console results => console.png
